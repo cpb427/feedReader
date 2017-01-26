cfee752b02eb3c6884f73d33e2d70549c5b1e2ee
Christopher Blanchard <CBlanchard@users-iMac.local>
2017-01-26 13:41:33 -0600

    added Awkward Zombie

--- server/server.js
+++ server/server.js
@@ -9,9 +9,9 @@ var util = require('util');
 var app = module.exports = loopback();
 var feedModel = app.models.feed;
 //TODO: move this into the database
-var arrayOfRSSFeeds = ['http://feeds.ign.com/ign/all', 'http://feeds.feedburner.com/CrackedRSS', 'http://www.theonion.com/feeds/rss', 'http://www.npr.org/rss/rss.php', 'http://feeds.feedburner.com/techcrunch', 'http://www.animenewsnetwork.com/news/rss.xml','http://comicbookrealm.com/rss/previews'];
-var sourceCheck = ['feeds.ign.com', 'feedproxy.google.com/~r/CrackedRSS', 'www.theonion.com', 'www.npr.org', 'feedproxy.google.com/~r/Techcrunch', 'www.wbur.org', 'www.animenewsnetwork.com','http://comicbookrealm.com'];
-var feedSources = ['IGN', 'Cracked','The Onion','NPR', 'TechCrunch', 'TechCrunch', 'Anime News', 'Comic Vine'];
+var arrayOfRSSFeeds = ['http://feeds.ign.com/ign/all', 'http://feeds.feedburner.com/CrackedRSS', 'http://www.theonion.com/feeds/rss', 'http://www.npr.org/rss/rss.php', 'http://feeds.feedburner.com/techcrunch', 'http://www.animenewsnetwork.com/news/rss.xml','http://comicbookrealm.com/rss/previews','http://awkwardzombie.com/awkward.php'];
+var sourceCheck = ['feeds.ign.com', 'feedproxy.google.com/~r/CrackedRSS', 'www.theonion.com', 'www.npr.org', 'feedproxy.google.com/~r/Techcrunch', 'www.wbur.org', 'www.animenewsnetwork.com','http://comicbookrealm.com', 'awkwardzombie.com'];
+var feedSources = ['IGN', 'Cracked','The Onion','NPR', 'TechCrunch', 'TechCrunch', 'Anime News', 'Comic Vine','Awkward Zombie'];
 var articles = [];
 
 //Make error logs print to a text file
@@ -31,7 +31,7 @@ var acquireBatchOfRSS = function() {
   	feed(arrayOfRSSFeeds[r], function(err, articles) {
     	if (err) {
 			console.log(error);
-			throw err;
+			throw err;  
 		}
     var currentArticle;
   	for(var i in articles) {
